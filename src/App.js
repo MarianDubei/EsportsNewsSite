473c6bf0057fa0a6346f6ee0a8e55c64b642c62f
MarianDubei <dubei@ucu.edu.ua>
2020-05-17 14:10:02 +0300

    added App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './components/page.css';
+import Header from './components/header';
+import Home from './components/home';
+import News from './components/news';
+import Schedule from './components/schedule';
+import About from './components/about';
+import Article from './components/article'
 
-import {NavBars, News, Schedule} from './page';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
 
-    <div className="container">
-      <NavBars/>
-      <div className="content">
-        <News/>
-        <Schedule/>
-      </div>
-    </div>
+  	<BrowserRouter>
+	    <div className="container">
+	    
+	      	<Header/>
+
+			<Switch>
+				<Route exact path='/' component={Home} />
+				<Route exact path='/news' component={News} />
+				<Route exact path='/schedule' component={Schedule} />
+				<Route exact path='/about' component={About} />
+				<Route exact path='/:game' render={(props) => (
+					<News game={props.match.params.game}/>
+					)}/>
+				<Route exact path='/:game/:articleLink' render={(props) => (
+					<Article game={props.match.params.game} articleLink={props.match.params.articleLink}/>
+					)}/>
+			</Switch>
+
+	    </div>
+    </BrowserRouter>
 
   );
 }
 
 export default App;
+
